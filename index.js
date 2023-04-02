86b34c8c8ed3aacd41fba76dffe7fb3cf30085ba
gopalkar <124944026+gopalkar@users.noreply.github.com>
2023-04-02 22:00:38 +0000

    Configured Linting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Load the http module to create an http server.
-import http from 'http';
+import http from 'http'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,11 +7,11 @@ dotenv.config()
 const port = process.env.PORT
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello  EWD!');
-});
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello  EWD!')
+})
 
-server.listen(port);
+server.listen(port)
 
 // Put a friendly message on the terminal
-console.log(`Server running at ${port}`);
+console.log(`Server running at ${port}`)
